f528d41865fc12b97a973d95fb5e38f69b5fdc8b
Enrique Martinez <emarfer786p@g.educaand.es>
2025-11-25 20:33:27 +0100

    Añado suma

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int a = 1;
+        int b = 2;
+
+        int result  = a+b;
+        System.out.println(result);
     }
 }
